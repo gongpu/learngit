93613887be5f9070a0e8e31b1d44ed0aa72de4ad
micheal <904582819@163.com>
2014-07-18 23:46:56 +0800

    modify mark

--- T.java
+++ T.java
@@ -1,16 +1,16 @@
 /**
-  * a tool class 
-  * @author micheal
-  * @time 2014-7-18
-  * 
-  */
+ * a tool class 
+ * @author micheal
+ * @time 2014-7-18
+ * 
+ */
 class T{
      /**  cut the content by the length
-       * @param content
-       * @param length
-       * return 
-       *
-       */ 
+      * @param content
+      * @param length
+      * return 
+      *
+      */ 
     public static String cutString(String content,int length){
         int len = content.length();
 	if(length > len){
@@ -21,9 +21,9 @@ class T{
 
     }
     /**
-      * some test methods
-      *
-      */
+     * some test methods
+     *
+     */
     public static void main(String args[]){
         String content = "I am from guangzhou where is beauful city!";
 	System.out.println(cutString(content,5));
