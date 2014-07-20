5e7f26bdb52fb9c7752dd18f607a14d67b57d946
micheal <904582819@163.com>
2014-07-20 15:44:52 +0800

    add ShowProperties.java

---
+++ ShowProperties.java
@@ -0,0 +1,15 @@
+/**
+ *:object/ShowProperties.java
+ * @author micheal
+ * @author https://github.com/gongpu/
+ *
+ */
+class ShowProperties{
+   public static void main(String args[]){
+       System.getProperties().list(System.out);
+       System.out.println(System.getProperty("user.name"));
+       System.out.println(
+           System.getProperty("java.library.path")
+       );
+   }    
+}
